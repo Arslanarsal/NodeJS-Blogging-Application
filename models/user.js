bb4fe1f1c158b6983d95ad0c8f30797f2442cedc
Arslan <arslanarsal455@gmail.com>
2025-06-23 14:23:42 +0500

    Add models

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { createHmac, randomBytes } = require('node:crypto');
+const mongoose = require('mongoose');
+
+const userScheme = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    salt: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImgULR: {
+        type: String,
+        default: "/images/default.png"
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    }
+})
+
+
+userScheme.pre("save", function (next) {
+    const user = this;
+    if (!user.isModified("password")) return;
+    const salt = randomBytes(16).toString();
+    const hash = createHmac('sha256', salt)
+        .update(user.password)
+        .digest('hex');
+    this.salt = salt;
+    this.password = hash;
+    next();
+
+})
+
+
+
+const user = mongoose.model("user", userScheme)
+module.exports = user
